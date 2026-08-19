0ba3d1de9f975c89318fdb21ed16734c3bde1e8e
54369-sudo <54369@yupparaj.ac.th>
2026-08-19 15:00:27 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -8,18 +8,12 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
-if "ans3_val" not in st.session_state:
-    st.session_state.ans3_val = ""
-if "ans4_val" not in st.session_state:
-    st.session_state.ans4_val = ""
 
 
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
-    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
-    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
 
@@ -28,14 +22,12 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2, ans3, ans4):
+def show_result_dialog(ans1, ans2):
     st.balloons()
     score = 0
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
-    u_ans3 = ans3.strip().lower()
-    u_ans4 = ans4.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -51,23 +43,9 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-    # ตรวจข้อ 3
-    if u_ans3 == "dog":
-        st.success("✅ ข้อ 3: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
-
-    # ตรวจข้อ 4
-    if u_ans4 == "banana":
-        st.success("✅ ข้อ 4: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
-
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 4:
+    if score == 2:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
@@ -99,21 +77,10 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-ans3 = st.text_input(
-    "ข้อ 3: A `d _ g` is man's best friend. 🐶",
-    value=st.session_state.ans3_val,
-)
-ans4 = st.text_input(
-    "ข้อ 4: Monkeys like to eat `b _ n _ n _`. 🍌",
-    value=st.session_state.ans4_val,
-)
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
-
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
@@ -126,7 +93,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2)
 
 st.divider()
 st.write("นาย กฤตยชญ์ อุ่นคำ เลขที่ 22 ม.4/8")
